d35db7919f84a88ea6af54d67b98c5e925275ef3
Владислав Львович Быконь <Hollow-oren@yandex.ru>
2017-11-30 21:12:01 +0500

    refactoring(in progress)

--- src/MyGame.php
+++ src/MyGame.php
@@ -4,36 +4,46 @@
 namespace Hollow93\Web2;
 
 
-
 class MyGame
 {
     private $frame;
     private $scorse;
 
 
-    public function getSumAll($scorse,$attempts)
+    public function getSumAll($scorse, $attempts)
     {
-
-        $isSpare=0;
-        $isStrike=0;
-        for ($i=0; $i<$attempts-1; $i++){
-            if ($scorse[$i] + $scorse[$i+1] === 10)
-            {
-                $isSpare += $scorse[$i+2];
-                $i+= 2;
+        $isSpare = 0;
+        $isStrike = 0;
+        for ($i = 0; $i < $attempts - 1; $i++) {
+            if ($this->isSpare($scorse, $i)) {
+                $isSpare += $scorse[$i + 2];
+                $i += 2;
             }
-            if ($scorse[$i] === 10)
-            {
-                $isStrike += $scorse[$i+1] + $scorse[$i+2];
+            if ($this->isStrike($scorse, $i)) {
+                $isStrike += $scorse[$i + 1] + $scorse[$i + 2];
             }
         }
-        if(is_array($scorse)) {
-            return array_sum($scorse) + $isSpare + $isStrike;
-        }
-            else
-            {
-            return $scorse;
-        }
+        return array_sum($scorse) + $isSpare + $isStrike;
     }
-    
+
+    /**
+     * @param $scorse
+     * @param $i
+     * @return bool
+     */
+    public function isSpare($scorse, $i): bool
+    {
+        return $scorse[$i] + $scorse[$i + 1] === 10;
+    }
+
+    /**
+     * @param $scorse
+     * @param $i
+     * @return bool
+     */
+    public function isStrike($scorse, $i): bool
+    {
+        return $scorse[$i] === 10;
+    }
+
 }
--- tests/MyGameTest.php
+++ tests/MyGameTest.php
@@ -18,52 +18,60 @@ class MyGameTest extends TestCase
     public function testEasyGame()
     {
         $input = new MyGame;
-        $attempts = 20 ;
-        $pins= array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-        $scorse = $input->getSumAll($pins,$attempts);
-        self::assertEquals(0,$scorse);
+        $attempts = 20;
+        $pins = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(0, $scorse);
     }
 
     public function testOneShot()
     {
         $input = new MyGame;
-        $attempts = 20 ;
-        $pins= array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-        $scorse = $input->getSumAll($pins,$attempts);
-        self::assertEquals(20,$scorse);
+        $attempts = 20;
+        $pins = array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(20, $scorse);
     }
 
     public function testSpare()
     {
         $input = new MyGame;
-        $attempts = 20 ;
-        $pins= array(4,6,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-        $scorse = $input->getSumAll($pins,$attempts);
-        self::assertEquals(35,$scorse);
+        $attempts = 20;
+        $pins = array(4, 6, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(35, $scorse);
     }
 
     public function testStrike()
     {
         $input = new MyGame;
-        $attempts = 19 ;
-        $pins= array(10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-        $scorse = $input->getSumAll($pins,$attempts);
-        self::assertEquals(30,$scorse);
+        $attempts = 19;
+        $pins = array(10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(30, $scorse);
     }
 
     public function testTwoStrike()
     {
         $input = new MyGame;
-        $attempts = 18 ;
-        $pins= array(10,10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-        $scorse = $input->getSumAll($pins,$attempts);
-        self::assertEquals(49,$scorse);
+        $attempts = 18;
+        $pins = array(10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(49, $scorse);
+    }
+
+    public function testFull()
+    {
+        $input = new MyGame;
+        $attempts = 18;
+        $pins = array(1, 1, 1, 1, 4, 6, 1, 1, 1, 1, 10, 10, 1, 1, 1, 1, 1, 1);
+        $scorse = $input->getSumAll($pins, $attempts);
+        self::assertEquals(58, $scorse);
     }
 
     /**
      * @return int
      */
-
 
 
     /**
